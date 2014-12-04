51f889bde5def479dc4595413116293689fc6161
Javier Carrillo <stringparser@gmail.com>
2014-12-04 22:58:28 +0100

    testing 1

--- test/children.js
+++ test/children.js
@@ -6,21 +6,23 @@ var hie = new pack({ name: rootName });
 
 it('should create nested structures', function(){
 
-  hie('hello there you').get('hello')
-    .should.be
-    .an.Object
-    .and.have
-    .property('name', 'hello');
+  hie('hello there you').get()
+    .should.be.an.Object
+    .and
+    .have.property('children');
 
-  hie.get('hello there')
-    .should.be
-    .an.Object
-    .and.have
-    .property('name', 'hello there');
+  hie.get('hello').children
+    .should.be.an.Object
+    .and
+    .have.property('there');
+
+  hie.get('hello there').children
+    .should.be.an.Object
+    .and
+    .have.property('you');
 
   hie.get('hello there you')
-    .should.be
-    .an.Object
-    .and.have
-    .property('name', 'hello there you');
+    .should.be.an.Object
+    .and
+    .not.have.property('children');
 });
--- test/parent.js
+++ test/parent.js
@@ -7,26 +7,18 @@ var hie = new pack({ name: rootName });
 it('should have proper parent', function(){
 
   hie.get()
-    .should.be
-    .an.Object
-    .and.have
-    .property('parent', undefined);
-    
+    .should.be.an.Object
+    .and.have.property('parent', undefined);
+
   hie.get('hello')
-    .should.be
-    .an.Object
-    .and.have
-    .property('parent', 'parentTest');
+    .should.be.an.Object
+    .and.have.property('parent', 'parentTest');
 
   hie.get('hello there')
-    .should.be
-    .an.Object
-    .and.have
-    .property('parent', 'hello');
+    .should.be.an.Object
+    .and.have.property('parent', 'hello');
 
   hie.get('hello there you')
-    .should.be
-    .an.Object
-    .and.have
-    .property('parent', 'hello there');
+    .should.be.an.Object
+    .and.have.property('parent', 'hello there');
 });
