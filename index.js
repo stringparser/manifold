83fa82347518b1f363b6146d21f4476766ef8051
Javier Carrillo <stringparser@gmail.com>
2015-01-12 22:54:35 +0100

    o -> opt: be obvious for argument names

--- index.js
+++ index.js
@@ -73,28 +73,25 @@ function Manifold(o){
 // return `this`
 //
 
-Manifold.prototype.set = function(stems, o){
+Manifold.prototype.set = function(stems, opt){
   var stemsIs = util.type(stems);
   if( !stemsIs.match(/function|string|array|object/g) ){
    throw new util.Error('set(stems [,options]) \n'+
      '> stems should be: `string`, `array`, `function` or `object`');
   }
 
-  var oIs = util.type(o);
-  o = oIs.plainObject || stemsIs.plainObject || { };
-  o.handle = stemsIs.function || oIs.function;
+  var optIs = util.type(opt);
+  opt = optIs.plainObject || stemsIs.plainObject || { };
+  opt.handle = stemsIs.function || optIs.function;
 
-  if(stemsIs.array || stemsIs.string){
-    stems = this.parth.set(stems).argv;
-  } else { stems = []; }
-
+  stems = (this.parth.set(stems) || '').argv || [ ];
   var node = this.store, parent = this.store;
-  stems.forEach(function createChildren(stem, index){
+  stems.forEach(function createChildren(stem){
     // ensure node existence
     if(!node.children){ node.children = { }; }
     if(!node.children[stem]){
       node.children[stem] = {
-        path: util.fold(stems.slice(0, index+1).join(' ')),
+        path: util.fold((node.path || '') + ' ' + stem),
         depth: node.depth + 1,
         parent: node.path || this.store.name
       };
@@ -104,16 +101,18 @@ Manifold.prototype.set = function(stems, o){
   }, this);
 
   // props of last node from its parent using options
-  Object.keys(o).forEach(function parseProps(name){
-    var value = util.clone(o[name], true);
+  Object.keys(opt).forEach(function parseProps(name){
+    var value = util.clone(opt[name], true);
     if(value === void 0){ return ; }
-    if(value === null){ delete node[name]; }
     if(this.parse.prop[name]){
-      this.parse(name)(parent, stems.slice(), value);
-    } else if(util.type(value).plainObject){
-      node[name] = node[name] || { };
+      return this.parse(name)(parent, stems.slice(), value);
+    }
+
+    node[name] = node[name] || { };
+    if(util.type(value).plainObject){
       util.merge(node[name], value);
     } else { node[name] = value; }
+
   }, this);
 
   return this;
@@ -124,23 +123,22 @@ Manifold.prototype.set = function(stems, o){
 //
 // arguments
 //  - `stems` type `string` or `array`
-//  - `o` type `object` optional holding all extra information
+//  - `opt` type `object` optional holding all extra information
 //
-// return node `found`
+// return
+//  - `found` node
 // --
 // api.public
 // --
 //
-Manifold.prototype.get = function(stems, o){
+Manifold.prototype.get = function(stems, opt){
   var stem, index, found;
-  o = o || util.type(stems).plainObject || { };
+  opt = util.type(opt || stems).plainObject || { };
 
   index = 0;
-  stems = this.parth.get(stems, o);
-
-  if(stems || o.argv){
-    stems = (stems || o).argv;
-  } else { index = -1; }
+  stems = this.parth.get(stems, opt);
+  if(stems || opt.argv){ stems = (stems || opt).argv; }
+  else { index = -1; }
 
   found = this.store;
   while(index > -1){ // always failback
@@ -150,11 +148,9 @@ Manifold.prototype.get = function(stems, o){
     } else { index = -1; }
   }
 
-  if(o.ref){ return found; }
+  if(opt.ref){ return found; }
 
-  stem = o.path;
-  util.merge(o, found);
-  delete o.children;
-  o.path = stem;
-  return util.clone(o, true);
+  util.merge(opt, found);
+  delete opt.children;
+  return util.clone(opt, true);
 };
