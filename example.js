939a98b620d7f715e551dd710be2b1e8a104280e
Javier Carrillo <stringparser@gmail.com>
2015-03-21 15:47:26 +0100

    readme examples

--- example.js
+++ example.js
@@ -2,52 +2,25 @@
 
 var app = require('./.')();
 
-function irand(){
-  return Math.floor(Math.random()*10);
-}
-
 var input = [
-  'get view.things',
-  'get view',
-  'get view.page',
-  '(post|get) :page.view',
-  'get page',
-  'get',
-  'get /:page/view',
-  'get /',
-  'get /:page',
-  'get /:page/:view'
-].sort(function(){
-  return irand()*Math.pow(-1, irand());
+  {
+    path: 'get /user/:page(\\d+)', opt: {
+      parent: 'get /user',
+      handle: function getUserPage(){}
+    },
+  },
+  {
+    path: 'get /user', opt: {
+      picture: function getPicture(){},
+      render: function markup(){}
+    }
+  }
+].map(function(input){
+  app.set(input.path, input.opt);
+  return input;
 });
 
 console.log(input);
 console.log(' ---- ');
 
-function pp(obj, sep){
-  if(!obj){ return '[nope]'; }
-  return JSON.stringify(obj, null, sep || ' ');
-}
-
-input.forEach(app.set.bind(app));
-Object.keys(app.store.children).forEach(function(path){
-  var node = this[path];
-  console.log('stem', node);
-  console.log('regex', node.regex);
-  console.log('parent', pp(node.parent));
-  console.log('children', pp(node.children));
-  console.log(' ---- ');
-}, app.store.children);
-
-app.set('get', function getHandle(){});
-
-app.set({name: 'test'});
-
-app.set('get /:page/:view', {
-  children: ['one']
-});
-
-console.log(app.get('get /user/profile'));
-console.log(app.get('get'));
-
-console.log(app);
+console.log(app.get('get /user/10'));
