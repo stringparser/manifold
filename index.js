bc2f47d391f0f981a73a68b67b2c5edaad0bd3b7
Javier Carrillo <stringparser@gmail.com>
2015-08-23 22:00:18 +0200

    just do a coumple things not everything

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var util = require('./lib/util');
+var Parth = require('parth');
 
 exports = module.exports = Manifold;
 
@@ -12,13 +13,10 @@ function Manifold(){
     return new Manifold();
   }
 
-  util.Parth.call(this);
-  util.defineProperty(this, 'parses', '', {});
-  util.defineProperty(this.store, 'children', 'e', {});
-
-  this.parse(util.defaultParsers.prop);
+  this.root = {};
+  Parth.call(this);
 }
-util.inherits(Manifold, util.Parth);
+util.inherits(Manifold, Parth);
 
 /* ## manifold.set([path, props])
 > set a path to regex mapping for an object
@@ -40,88 +38,33 @@ So you know... interesting things can happen.
 */
 
 Manifold.prototype.set = function(path, opt){
-  if(!util.type(path).match(/string|function|plainObject/)){
-    throw new TypeError('set(path/options [, options]). '+
-      'Unsupported type for path/options.\n\n'+
-      'Supported types are:\n'+
-      ' - string (path)\n' +
-      ' - plainObject (options)\n'
-    );
+  opt = opt || path;
+  var o = util.type(opt || path).plainObject || {};
+
+  o.path = util.type(o.path || path).string;
+  o.handle = util.type(o.handle || opt).function;
+  o.parent = util.type(o.parent).string;
+
+  var node = this.root;
+  if(o.path && !node[o.path]){
+    util.super(this, 'set')(o.path);
+    node = this.store[o.path];
+  } else if(this.store[o.path]){
+    node = this.store[o.path];
   }
 
-  var oType = util.type(opt || path);
-  var o = oType.plainObject || '';
-
-  if(util.type(path || o.path).string){
-    if(!o){ o = {}; } o.path = path || o.path;
+  if(o.parent && o.parent !== o.path && this.store[o.parent]){
+    node.parent = this.store[o.parent];
+    o.parent = null;
   }
 
-  if(oType.function || util.type(o.handle).function){
-    if(!o){ o = {}; } o.handle = oType.function || o.handle;
-  }
-
-  var node = this.store;
-  var stem = util.boil(o.path);
-  if(stem && !node.children[stem.path]){
-    util.super(this, 'set')(stem.path);
-    node = node.children[stem.path];
-    util.defineProperty(node, 'parent', 'w', this.store);
-    util.defineProperty(node, 'children', 'w');
-  } else if(stem){
-    node = node.children[stem.path];
-  }
-
-  return this.parse(node, o);
-};
-
-/* ### manifold.parse(prop[, parser])
-> parse `node` properties _before_ they are set
-
-The method sets a parser for latter usage in
-[`manifold.set([path, props]`](#manifoldsetpath-props).
-The parser function will be invoked when  `props`
-of that #set method has a property named `prop`.
-
-_arguments_
- - `prop`, type string or object with one function per key
- - `parser`, optional, type `function`
-
-_returns_
- - `parser` for less than two arguments
- - `this` for two arguments
-*/
-Manifold.prototype.parse = function(node, props){
-
-  if(typeof props === 'function'){
-    var self = this, parser = props;
-    this.parses[node] = function parseProp(/* arguments */){
-      return parser.apply(self, arguments);
-    };
-    return this;
-  } else if(!props && util.type(node).object){
-    Object.keys(node).forEach(function(key){
-      this.parse(key, node[key]);
-    }, this);
-    return this;
-  } else if(!props){
-    throw new TypeError(
-      'parse(object node[, object props]) node is not an object'
-    );
-  } else if(!util.type(props).object){
-    throw new TypeError(
-      'parse(object node, object props) props is not an object'
-    );
-  }
-
-  Object.keys(props).forEach(function parseOptions(key){
-    var value = props[key];
-    if(this.parses[key]){
-      return this.parses[key](node, value, props);
-    } else if(util.type(value).plainObject){
+  Object.keys(o).forEach(function(key){
+    var value = o[key]; if(!value){ return; }
+    if(util.type(value).plainObject){
       if(!node[key]){ node[key] = {}; }
       util.merge(node[key], util.clone(value, true));
     } else {
-      node[key] = util.clone(value, true);
+      node[key] = util.clone(o[key], true);
     }
   }, this);
 
@@ -129,36 +72,33 @@ Manifold.prototype.parse = function(node, props){
 };
 
 /*
-## manifold.get([path, options, mod])
-> get an object matching the given path, clone it if necessary
+## manifold.get
+```js
+function get([string path, object options])
+```
+Get an object previously set matching the `path` given.
 
 _arguments_
  - `path`, optional, type string
  - `options`, optional, type object with all extra information
- - `mod`, type object. If is a:
-   - plainObject with property ref, the node found will not be cloned
-   - regular expression, are the props to skip while cloning
+
+_when_
+ - `options.ref` is `true` the original node is returned
+ - the node found has a parent property the parent properties are cloned into the returned object
 
 _returns_ the object (cloned/by reference) `node` found
-
-In addition, if the node has a parent it will inherit its
-properties while cloning.
-
 */
 
 var skipRE = /children|parent/;
 
-Manifold.prototype.get = function(path, opt, mod){
-  var o = util.type(opt || path).object || {};
-  mod = mod || o;
-
-  var node = this.store;
+Manifold.prototype.get = function(path, opt){
+  var o = util.type(opt || path).plainObject || {};
   var stem = util.super(this, 'get')(path, o);
+  var node = stem ? this.store[stem.path] : this.root;
 
-  if(stem){ node = node.children[stem.path]; }
-  if(mod && mod.ref){ return node; }
+  if(o.ref){ return node; }
 
-  function whilst(){
+  (function whilst(){
     Object.keys(node).forEach(function(key){
       if(skipRE.test(key) || o[key] !== void 0){ return ; }
       o[key] = util.clone(node[key], true);
@@ -168,9 +108,7 @@ Manifold.prototype.get = function(path, opt, mod){
       node = node.parent;
       whilst();
     }
-  }
-
-  whilst();
+  })();
 
   return o;
 };
